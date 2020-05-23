5893cd079b99423d5f2810c2b9fec122b5da3e0f
Martin <m.grigorov@student.fontys.nl>
2020-05-23 12:45:23 +0300

    api in progress

--- SimulationSetting/dbconnect.php
+++ SimulationSetting/dbconnect.php
@@ -9,12 +9,33 @@ $dbuser = 'dbi400672';
 $dbpassword = 'ProCPLions';
 $db = 'dbi400672';
 $dbport = 3306;
+private $connection;
 
+/*
 $dbconnection = mysqli_connect($dbhost, $dbuser, $dbpassword, $db);
 
 if($dbconnection){
+    connect
 }
 else{
     echo("Connection failed " . mysql_error());
 }
+*/
+///
+
+public function connect() {
+
+    $this->connection = null;
+
+    try { //dsn - database type and host, takes all necessary parametres from above in order to connect
+        $this->connection = new PDO('mysql:host=' . $this->dbhost . ';dbname=' . $this->db, $this->dbuser, $this->dbpassword);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e){
+        echo 'Connection Error: ' . $e->getMessage();
+    }
+
+    return $this->connection;
+}
+
 ?>
