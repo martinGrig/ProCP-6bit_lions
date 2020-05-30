1fb98aaa0bb713bd26c9039b2ed656b4a02d4ee7
Martin <m.grigorov@student.fontys.nl>
2020-05-30 13:39:09 +0300

    Fi

---
+++ ShopPositionOptimization/ShopPositionOptimization/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherScript
+{
+    class Result
+    {
+        private List<Shop> shops;
+        private string _demographic;
+        private double _incomePerSimulation;
+        private TimeSpan _busyTime;
+
+        public Result(List<Shop> perShopVisitRate, double incomePerSimulation, TimeSpan busyTime)
+        {
+            //_demographic = demographic;
+            shops = new List<Shop>();
+            _incomePerSimulation = incomePerSimulation;
+            _busyTime = busyTime;
+        }
+    }
+}
