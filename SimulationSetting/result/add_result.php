67258dfa38984c8d7db97713f713cd4b39282e57
kimsis <mimaqm@gmail.com>
2020-05-30 14:22:00 +0300

    some php changes

--- SimulationSetting/result/add_result.php
+++ SimulationSetting/result/add_result.php
@@ -1,11 +1,20 @@
 <?php
 include 'dbconnect.php';
 
-$sql = "INSERT INTO result (ID, TotalIncome, TotalVisitors, Duration) VALUES (?,?,?,?)";
+$sql = "INSERT INTO result (TotalIncome, TotalVisitors, Duration) VALUES (?,?,?);";
 $stmt = $dblink->prepare($sql);
-$stmt->bind_param("idis", $_POST['ID'], $_POST['TotalIncome'], $_POST['TotalVisitors'], $_POST['Duration']);
+$stmt->bind_param("dis", $_POST['TotalIncome'], $_POST['TotalVisitors'], $_POST['Duration']);
 $stmt->execute();
+
+$result = $dblink->query("SELECT LAST_INSERT_ID();");
+$row = $result->fetch_assoc();
+foreach($row as $key=>$value)
+$resultID = $value;
+
 $stmt->close();
 $dblink->close();
 
+//Print array in JSON format
+echo $resultID;
+header('Content-Type: application/json');
 ?>
