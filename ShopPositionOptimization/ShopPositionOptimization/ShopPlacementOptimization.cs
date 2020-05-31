42eda27b77068510797932786724a39abc5872de
Georgieva, Nikoleta N.Z <n.georgieva@student.fontys.nl>
2020-05-31 09:30:34 +0200

    Script for shop optimization, v1

--- ShopPositionOptimization/ShopPositionOptimization/ShopPlacementOptimization.cs
+++ ShopPositionOptimization/ShopPositionOptimization/ShopPlacementOptimization.cs
@@ -4,6 +4,7 @@ using System.Drawing.Text;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using WeatherScript;
+using System.Linq;
 
 namespace ShopPositionOptimization
 {
@@ -12,7 +13,7 @@ namespace ShopPositionOptimization
         
         private Position[] positions;
         private Result[] shopResults;
-        private Dictionary<int, string> shopPositions; //Shop, Position
+        private Dictionary<int, string> shopPositions; //Position, shopName
         private Dictionary<int, double> profitsPerPosition; //Position, Profit
         private List<Object> positionShopResultList;
 
@@ -44,76 +45,69 @@ namespace ShopPositionOptimization
 
         public Dictionary<int, string>  OptimizeShopPlacement(string WhoKnowsWhat)
         {
-            //Sort(shopResults, IComparer)  //sort by position
-            //double[] shopRevenues;
-            //string[] shopNames;
+            Sort(shopResults, IComparer);  //sort by position, not done yet
 
-            //if (shopResults.Lenght >= 5)
-            //{
+            double[] shopRevenues = 5;
+            List<string> shopNames = 5;
 
-            //for i=0; i<shopResults.Length; i++ // this for have to loop through all the results returned from the database
+            if (shopResults.Lenght >= 15)
+            {
 
-            //switch(shopResults[i].positionId) // go through all the results and compare the revenue to see which shop is most suitable for what possition
+                for (int i = 0; i < shopResults.Length; i++)
+                {
+                    switch (shopResults[i].positionID)
+                    {
+                        case 1:
+                            if (shopRevenues[0] < shopResults[i].revenue)
+                            {
+                                shopRevenues[0] = shopResults[i].revenue;
+                                shopNames.ElementAt(0) = shopResults[i].shopname;                                
+                            }
+                            break;
 
-            //case position = 1
-            //if (shopRevenues[position-1]<row.revenue)
-            //shopRevenues[position-1]=row.revenue //change the highest income for position 1
-            //shopNames[position-1] = row.shopname // change the highest income shop for position 1
-
-            //case position = 2
-            //if (shopRevenue[position-1]<row.revenue)
-            //shopRevenues[position-1]=row.revenue //change the highest income for position 1
-            //shopNames[position-1] = row.shopname // change the highest income shop for position 1
-
-            //case position = 3
-            //if (max3<row.revenue)
-            //max3=row.revenue
-            //shop3 = row.shopname
-            //shopPositions.Add(3, row.shopname);
-
-            //case position = 4
-            //if (max3<row.revenue)
-            //max3=row.revenue
-            //shop3 = row.shopname
-            //shopPositions.Add(4, row.shopname);
-
-            //case position = 5
-            //if (max3<row.revenue)
-            //max3=row.revenue
-            //shop3 = row.shopname
-            //shopPositions.Add(5, row.shopname);
-
-            //foreach(KeyValuePair<int, string> entry in shopPositions)
-            // {
-            //     if  (entry.Key == 1 && entry.Value != row.shopname && max1 < row.revenue)
-            //     {
-            //         //max1=row.revenue
-            //         //shop1 = row.shopname
-            //     }
-            //     if (entry.Key == 2 && entry.Value != row.shopname && max2 < row.revenue)
-            //     {
-            //         //max1=row.revenue
-            //         //shop1 = row.shopname
-            //     }
-            //     if (entry.Key == 3 && entry.Value != row.shopname && max3 < row.revenue)
-            //     {
-            //         //max1=row.revenue
-            //         //shop1 = row.shopname
-            //     }
-            //     if (entry.Key == 4 && entry.Value != row.shopname && max4 < row.revenue)
-            //     {
-            //         //max1=row.revenue
-            //         //shop1 = row.shopname
-            //     }
-            //     if (entry.Key == 5 && entry.Value != row.shopname && max5 < row.revenue)
-            //     {
-            //         //max1=row.revenue
-            //         //shop1 = row.shopname
-            //     }
-
-            // }
+                        case 2:
+                            if (shopRevenues[1] < shopResults[i].revenue && !shopNames.Contains(shopResults[i].shopName))
+                            {
+                                shopRevenues[1] = shopResults[i].revenue;
+                                shopNames.ElementAt(1) = shopResults[i].shopname;
+                            }
+                            break;
 
 
+                        case 3:
+                            if (shopRevenues[2] < shopResults[i].revenue && !shopNames.Contains(shopResults[i].shopName))
+                            {
+                                shopRevenues[2] = shopResults[i].revenue;
+                                shopNames.ElementAt(2) = shopResults[i].shopname;
+                            }
+                            break;
+
+
+                        case 4:
+                            if (shopRevenues[3] < shopResults[i].revenue && !shopNames.Contains(shopResults[i].shopName))
+                            {
+                                shopRevenues[3] = shopResults[i].revenue;
+                                shopNames.ElementAt(3) = shopResults[i].shopname;
+                            }
+                            break;
+
+
+                        case 5:
+                            if (shopRevenues[4] < shopResults[i].revenue && !shopNames.Contains(shopResults[i].shopName))
+                            {
+                                shopRevenues[4] = shopResults[i].revenue;
+                                shopNames.ElementAt(4) = shopResults[i].shopname;
+                            }
+                            break;
+                    }
+                }
+
+            for (int i = 0; i < shopNames.Count; i++)
+            {
+                shopPositions.Add(i + 1, shopNames.ElementAt(i));
+            }
+
+                    
             //////////////////////////////////////////
             //shopPositions.Add(1, row.shopname);
 
