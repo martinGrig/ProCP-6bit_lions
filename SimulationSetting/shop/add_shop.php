f234ff95f9227cf6f1b6567fbcd53f165948e87d
kimsis <mimaqm@gmail.com>
2020-06-20 17:52:52 +0300

    Changed update_shop and add_shop, renamed gets

--- SimulationSetting/shop/add_shop.php
+++ SimulationSetting/shop/add_shop.php
@@ -1,9 +1,9 @@
 <?php
 include 'dbconnect.php';
 
-$sql = "INSERT INTO shop ( Name, Popularity, PriceRange, BusyHours, Category) VALUES (?,?,?,?,?)";
+$sql = "INSERT INTO shop ( Name, AverageSpending, BusyHours, Category) VALUES (?,?,?,?)";
 $stmt = $dblink->prepare($sql);
-$stmt->bind_param("sddss", $_POST['Name'], $_POST['Popularity'], $_POST['PriceRange'], $_POST['BusyHours'], $_POST['Category']);
+$stmt->bind_param("sdss", $_POST['Name'], $_POST['AverageSpending'], $_POST['BusyHours'], $_POST['Category']);
 $stmt->execute();
 $stmt->close();
 $dblink->close();
--- SimulationSetting/shop/update_shop.php
+++ SimulationSetting/shop/update_shop.php
@@ -1,13 +1,13 @@
 <?php
 include 'dbconnect.php';
 
+$OldName = $_POST['OldName'];
 $Name = $_POST['Name'];
-$Popularity = $_POST['Popularity'];
-$PriceRange = $_POST['PriceRange'];
+$AverageSpending = $_POST['AverageSpending'];
 $BusyHours = $_POST['BusyHours'];
 $Category = $_POST['Category'];
-$stmt = $dblink->prepare("UPDATE shop SET Popularity = ?, PriceRange = ?, BusyHours = ?, Category = ? WHERE Name = ?");
-$stmt->bind_param("ddsss", $Popularity, $PriceRange, $BusyHours, $Category, $Name);
+$stmt = $dblink->prepare("UPDATE shop SET Name = ?, AverageSpending = ?, BusyHours = ?, Category = ? WHERE Name = ?");
+$stmt->bind_param("sdsss", $Name, $AverageSpending, $BusyHours, $Category, $OldName);
 $stmt->execute();
 $stmt->close();
 $dblink->close();
